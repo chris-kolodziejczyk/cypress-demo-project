110dc88c633416dc014da1a79038e415d6842961
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2021-09-17 12:24:32 +0200

    Utils class minor update

--- cypress/support/utils/utils.js
+++ cypress/support/utils/utils.js
@@ -1,6 +1,8 @@
 export default class Utils {
-	// Get fixture data from provided file in fixturePath parameters,
-	// @param: fixturePath - path to fixture file,
+
+/** Get fixture data from provided file in fixturePath parameters
+ * @param {string} fixturePath - path to fixture file
+ */
 	static getFixtureData(fixturePath) {
 		return cy.fixture(fixturePath).then((data) => {
 			return data;
